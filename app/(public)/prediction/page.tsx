cd8acb098d80f6e4f685c812986be8797f81679d
iqbal-mn694 <garenahiber134@gmail.com>
2026-07-28 07:32:16 +0700

    add fase tanam chart

--- app/(public)/prediction/page.tsx
+++ app/(public)/prediction/page.tsx
@@ -1,10 +1,16 @@
 import InputFile from "@/components/pages/prediction-page/input-file";
+import FaseTanamChart from "@/components/pages/prediction-page/FaseTanamChart";
 import React from "react";
 
 const Prediction = () => {
   return (
-    <div className="mt-20">
-      <InputFile />
+    <div className="mt-20 space-y-10">
+      <section>
+        <FaseTanamChart />
+      </section>
+      <section>
+        <InputFile />
+      </section>
     </div>
   );
 };
